3d5cd23f1946acc37b6320cb3f637871bfe0be85
Ryan <63581031+ryantzr1@users.noreply.github.com>
2023-01-14 16:11:09 +0800

    Updating Index.js with the runMLModel Script and /predict
    /predict is just a sample for how we can call the runMLScript function 
    //Wei Hern -- need to input the python file in the runMLScript argument
    
    Gerald can see how you want to make use of the /predict function

--- Telegram Bot/index.js
+++ Telegram Bot/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const axios = require('axios')
 const express = require('express')
 const parser = require('body-parser')
+// const { spawn } = require("child_process"); 
 
 const { TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
@@ -42,6 +43,53 @@ app.post(link, async (req, res) => {
     }
     return res.send()
 })
+// // runMLScript runs our ML model when a new conversation with the customer starts
+// async function runMLScript(parameter) {
+//   return new Promise((resolve, reject) => {
+//     const python = spawn("python3", [
+//       "", ///To fill in with the script name!!! 
+//       parameter,
+//     ]);
+//     let output = "";
+//     python.stdout.on("data", (data) => {
+//       console.log(`stdout: ${data}`);
+//       output += data;
+//     });
+
+//     python.stderr.on("data", (data) => {
+//       // console.log(`stderr: ${data}`);
+//     });
+
+//     python.on("close", (code) => {
+//       // console.log(`child process exited with code ${code}`);
+//       const results = output;
+//       resolve(results);
+//     });
+//   });
+// }
+
+
+// app.post("/predict", async (req, res) => {
+//   console.log("Making Predictions now");
+//     try {
+//         const text = req.body.message.text
+//         const results = await runMLScript(text); //running ML script to get the results
+//         console.log(results);
+//         await axios.post(`${TELEGRAM_API}/sendMessage`, {
+//                     chat_id: userId,
+//                     text: "Hello " + text
+//                 })               //Gerald will need you to check this sendMessage Function
+//      } 
+//      catch (error) {
+//         console.error(error);
+//         res.status(500).send({ error: "An error occurred" });
+//      }
+//      return res.send()
+//}) 
+
+
+
+
 
 const init = async () => {
     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${MAIN_URL}`)
