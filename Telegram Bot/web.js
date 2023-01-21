b872fa68a028b8947e053f7fe63e82a231cffffa
Ryan <dreisprung1821@gmail.com>
2023-01-21 15:52:08 +0800

    Updated with MongoDB integration

--- Telegram Bot/web.js
+++ Telegram Bot/web.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const packageInfo = require('./package.json');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const packageInfo = require("./package.json");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+//making connection to MongoDB server
+//MONGO_URL = "mongodb+srv://tanaldo:hello123@cluster0.b1cjijc.mongodb.net/Friday"
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to your database"))
+  .catch((err) => console.log(err));
+
+app.get("/", function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
 var server = app.listen(process.env.PORT, "0.0.0.0", () => {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Web server started at http://%s:%s', host, port);
+  console.log("Web server started at http://%s:%s", host, port);
 });
 
 module.exports = (bot) => {
-  app.post('/' + bot.token, (req, res) => {
+  app.post("/" + bot.token, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
   });
 };
-
-
-
-
-
