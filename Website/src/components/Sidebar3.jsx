5ac1f3ec054dc6da60e7b754b76f1627cab242f4
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-01-25 20:01:15 +0800

    initial commit

---
+++ Website/src/components/Sidebar3.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { Link } from 'react-router-dom';
+import { SidebarData } from "../Data/Data";
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
+import Logo from "../imgs/icon3.png";
+
+const Sidebar3 = () => {
+  const [current, setCurrent] = useState(2);
+
+  const [enlarged, setEnlarged] = useState(true)
+
+  const sidebarVariants = {
+    true: {
+      left : '2'
+    },
+    false:{
+      left : '-59%'
+    }
+  }
+  console.log(window.innerWidth)
+  return (
+    <>
+      <div className="bars" style={enlarged?{left: '60%'}:{left: '5%'}} onClick={()=>setEnlarged(!enlarged)}>
+        <UilBars />
+      </div>
+    <motion.div className='sidebar'
+    variants={sidebarVariants}
+    animate={window.innerWidth<=768?`${enlarged}`:''}
+    >
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+        <span>
+          F.R.I.<span>D.A.Y.</span>
+        </span>
+      </div>
+      <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <Link
+              className={current === index ? "menuItem active" : "menuItem"}
+              key={index}
+              onClick={() => setCurrent(index)}
+              to={"/" + item.url}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </motion.div>
+    </>
+  );
+};
+
+export default Sidebar3;
