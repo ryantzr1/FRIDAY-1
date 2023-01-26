ceeb8f6df37f159a3f3731d9c4503289a50f8333
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-01-26 09:52:52 +0800

    initial commit

--- Telegram Bot/bot.js
+++ Telegram Bot/bot.js
@@ -16,21 +16,19 @@ if (process.env.NODE_ENV === "production") {
 console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
 bot.on("message", async (msg) => {
-  // console.log(msg);
+  console.log(msg);
   try {
     const text = msg.text;
     console.log(text);
-    const res = await axios.get("http://13.115.58.243//predict", {
+    const res = await axios.get("http://13.115.58.243/predict", {
       params: { question: text },
     });
-    console.log(res.data.answer);
+    console.log(res.data);
     const answer = res.data.answer;
-    //we also want to store the question and answer in our mongoDB
 
     //a convo instance
-    const convo = new Convo({ question: text, answer: "hello" });
-    // console.log(convo);
-    //save model to database
+    const convo = new Convo({ question: text, answer: answer });
+    console.log(convo);
     convo.save(function (err, convo) {
       if (err) {
         return console.log(err);
@@ -38,12 +36,11 @@ bot.on("message", async (msg) => {
         console.log(convo.name + "saved to Convos collection");
       }
     });
-
-    // bot.sendMessage(msg.chat.id, res.data.answer).then(() => {});
-    bot.sendMessage(msg.chat.id, "hello").then(() => {});
+    bot.sendMessage(msg.chat.id, answer).then(() => {});
 
   } catch (e) {
     console.log(e.message);
+    bot.sendMessage(msg.chat.id, "Sorry. We are unable to process your query.").then(() => {});
   }
 });
 
