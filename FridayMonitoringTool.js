633abdbd78207b3fd5c76d01beef0637daec6c47
ryantzr1 <dreisprung1821@gmail.com>
2023-05-04 14:44:08 +0800

    Initial commit

--- FridayMonitoringTool.js
+++ FridayMonitoringTool.js
@@ -15,34 +15,15 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // Handle the /start command
-// Handle the /start command
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const chatType = msg.chat.type;
 
-  if (chatType === "private") {
+  if (chatType === "channel") {
+    // Check if user provided the correct password
     if (msg.text.split(" ")[1] === process.env.FRIDAYMONITORINGTOOL) {
-      bot.sendMessage(chatId, "Welcome! You have access to the bot.");
-      verifiedChatIds.add(chatId); // Add to verifiedChatIds array
-
-      const commands = [
-        {
-          command: "/troubleshoot",
-          description: "Guide to troubleshoot issues",
-        },
-        { command: "/reportbug", description: "Report Bugs" },
-        { command: "/newfeature", description: "Push a new feature!" },
-      ];
-
-      // const keyboard = {
-      //   keyboard: commands.map((command) => [{ text: command.command }]),
-      //   resize_keyboard: true,
-      //   one_time_keyboard: true,
-      // // };
-
-      // bot.sendMessage(chatId, "Available commands:", {
-      //   reply_markup: keyboard,
-      // });
+      // bot.sendMessage(chatId, "Welcome! You have access to the bot.");
+      verifiedChatIds.add(`@${msg.chat.username}`);
     } else {
       bot.sendMessage(
         chatId,
@@ -50,10 +31,38 @@ bot.onText(/\/start/, (msg) => {
       );
     }
   } else {
-    bot.sendMessage(
-      chatId,
-      "This bot is only available for private chats. Please contact the bot in a private chat."
-    );
+    // Private chat handling remains the same
+    if (msg.text.split(" ")[1] === process.env.FRIDAYMONITORINGTOOL) {
+      bot.sendMessage(chatId, "Welcome! You have access to the bot.");
+      verifiedChatIds.add(chatId); // Add to verifiedChatIds array
+
+      // const commands = [
+      //   {
+      //     command: "/troubleshoot",
+      //     description: "Guide to troubleshoot issues",
+      //   },
+      //   { command: "/reportbug", description: "Report Bugs" },
+      //   { command: "/newfeature", description: "Push a new feature!" },
+      // ];
+
+      // if (chatType === "private") {
+      //   // Check if the chat type is private
+      //   const keyboard = {
+      //     keyboard: commands.map((command) => [{ text: command.command }]),
+      //     resize_keyboard: true,
+      //     one_time_keyboard: true,
+      //   };
+
+      //   bot.sendMessage(chatId, "Available commands:", {
+      //     reply_markup: keyboard,
+      //   });
+      // }
+    } else {
+      bot.sendMessage(
+        chatId,
+        "Access denied. Please provide the correct password."
+      );
+    }
   }
 });
 
