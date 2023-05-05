cda30235a95a5ec933a7658f74920436efc484cf
ryantzr1 <dreisprung1821@gmail.com>
2023-05-05 23:20:31 +0800

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
-const bot = require("./bot");
-const db = require("./databaseHandle");
+// const express = require("express");
+// const app = express();
+// const bot = require("./bot");
+// const db = require("./databaseHandle");
 
-// Listen to the correct port specified by Heroku
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// // Listen to the correct port specified by Heroku
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
