01efe5d8ee79cdd74ce0c10c352aae3504317e6d
ryantzr1 <dreisprung1821@gmail.com>
2023-05-06 16:03:56 +0800

    My first commit

--- commands.js
+++ commands.js
@@ -283,27 +283,6 @@ function onNewFeature(msg) {
   }
 }
 
-async function trackMessages() {
-  const collection = db.collection("queries");
-  const changeStream = collection.watch();
-
-  changeStream.on("change", (next) => {
-    // Perform actions when a new object is inserted into the collection
-    const question = next.fullDocument.question;
-    const answer = next.fullDocument.answer;
-
-    // Notify all verified users of the new object
-    for (const userChatId of verifiedChatIds) {
-      bot.sendMessage(
-        userChatId,
-        `New question added:\n\nQuestion: ${question}\nAnswer: ${answer}`
-      );
-    }
-  });
-}
-
-trackMessages(); // Start tracking messages
-
 module.exports = {
   onStart,
   onTroubleshoot,
