8e481bac56e7cac7d08982c202ae656ab6bbf173
ryantzr1 <dreisprung1821@gmail.com>
2023-05-06 16:02:18 +0800

    My first commit

--- databaseHandle.js
+++ databaseHandle.js
@@ -1,9 +1,32 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true },
+  () => trackMessages() //this is how we can track all incoming messages
+);
 
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// module.exports = db;
+async function trackMessages() {
+  const collection = db.collection("queries");
+  const changeStream = collection.watch();
+
+  changeStream.on("change", (next) => {
+    // Perform actions when a new object is inserted into the collection
+    const question = next.fullDocument.question;
+    const answer = next.fullDocument.answer;
+
+    // Notify all verified users of the new object
+    for (const userChatId of verifiedChatIds) {
+      bot.sendMessage(
+        userChatId,
+        `New question added:\n\nQuestion: ${question}\nAnswer: ${answer}`
+      );
+    }
+  });
+}
+
+module.exports = db;
