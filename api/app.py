9612b67bcb43546a6751b1f1c04018b3cb986e24
nreHieW <limweihern@gmail.com>
2023-05-07 16:06:20 +0800

    v1.1

--- api/app.py
+++ api/app.py
@@ -1,31 +1,15 @@
-from fastapi import FastAPI
-from langchain.vectorstores import Pinecone
-from langchain.embeddings import HuggingFaceHubEmbeddings
 import openai
-import pinecone
 import os 
+import pytz
+from fastapi import FastAPI
 from api.api_keys import *
-from api.utils import *
+from Agents import *
+from Agents.utils import *
 
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ['PINECONE_API_ENV'] = PINECONE_API_ENV
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 DIMENSIONS = 768
-
-pinecone.init(api_key=os.environ["PINECONE_API_KEY"], api_env=os.environ['PINECONE_API_ENV'])
-index_name = "airbnb"
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
-embeddings = HuggingFaceHubEmbeddings()
-docsearch = Pinecone.from_existing_index(index_name, embeddings)
-
-### STATE ###
-
-STATE = [
-    {"role": "system", "content": system_prompt}
-]
-TOKENS = 0
+tz = pytz.timezone('Asia/Singapore')
 
 ### APP ###
 
@@ -33,25 +17,19 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Test": "Hello World"}
+    return {"status": "alive"}
 
 @app.get("/predict")
-def predict(query):
-    global STATE
-    ans, userprompt, STATE = generate_answer(query, docsearch, STATE, False)
-    reply = ans['choices'][0]['message']['content']
-    tokens = ans["usage"]["total_tokens"]
-    print("\n")
-
-    print(f"Total tokens used: {tokens}, Estimated cost: ${tokens/1000*0.002:0.5f}")
-
-    ## Time being 
-    STATE = [
-    {"role": "system", "content": system_prompt}
-    ]
-
+def predict(question: str, chat_history: list, company_info: dict) -> dict:
+    router = ToolSelectionAgent(company_info)
+    agent, tool_selection_usage = router.route(question, chat_history)
+    
+    ans_payload = agent.generate_answer(question, chat_history)
+    ans = ans_payload["answer"]
+    usage = sum_dict(tool_selection_usage, ans_payload["tool_selection_usage"])
     return {
-    "question": query,
-    "user_prompt": userprompt,
-    "answer": reply,
-}
+        "question": question,
+        "answer": ans,
+        "usage": usage,
+        "agent": str(agent)
+    }
