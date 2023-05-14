948d19f7b7a542f9cd73b95429400cb2dfe0bd8a
ryantzr1 <dreisprung1821@gmail.com>
2023-05-15 01:04:31 +0800

    Your commit message

--- DashcamSG.js
+++ DashcamSG.js
@@ -25,6 +25,8 @@ app.post("/respondio", (req, res) => {
   const messageText = req.body.message.message.text;
 
   console.log(`Received message from ${phoneNumber}: ${messageText}`);
+  // Log the entire request object
+  console.log(req);
 
   // Respond to the request to acknowledge receipt
   res.status(200).end();
