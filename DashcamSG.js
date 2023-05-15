915f42e1999e4a931c570df6e0f61b7437af5f68
ryantzr1 <dreisprung1821@gmail.com>
2023-05-15 23:41:58 +0800

    Your commit message

--- DashcamSG.js
+++ DashcamSG.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Define the MongoDB schema for storing queries
 const querySchema = new mongoose.Schema({
   userId: { type: String, required: true },
+  customerFirstName: { type: String, required: true },
   question: { type: String, required: true },
   answer: { type: String, required: true },
   success: { type: Boolean, required: true },
@@ -41,7 +42,10 @@ const apiUrl = "https://api.respond.io/v2";
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY3NCwic3BhY2VJZCI6MTQ0NDU0LCJvcmdJZCI6MTI1MDU2LCJ0eXBlIjoiYXBpIiwiaWF0IjoxNjgxMzE1MzUxfQ.XWGY5WbM9Rvp73zVpkdvLPa46BVoPoqFTiKzukKmK8M"; // Replace with your actual API token
 
-app.post("/respond.io", async (req, res) => {
+//whenever we close the convo, this updates the failureCount back to 0
+//this simulates how when DashcamSG closes the convo, FRIDAY will be there to answer
+//to get around this, on our dashboard we can show the user whom Dashcam has yet to answer!!!
+app.post("/conversationClosed", async (req, res) => {
   const signature = req.get("X-Webhook-Signature");
   const signingKey = "EpfaLRpQL9Iq6LuFbvbkrJICC5Qt4cZR8tkFmoumEvw=";
 
@@ -62,6 +66,39 @@ app.post("/respond.io", async (req, res) => {
   console.log(`Received message from ${userId}: ${messageText}`);
   console.log("This is the channelId" + " " + channelId);
 
+  //checking the previous failure count to update the current failure count
+  const mongoCustomer = await Query.findOne({ user: userId }).sort({
+    createdAt: -1,
+  });
+
+  mongoCustomer.failureCount = 0;
+  await mongoCustomer.save();
+  //ok now reset the failureCount
+});
+
+app.post("/respond.io", async (req, res) => {
+  const signature = req.get("X-Webhook-Signature");
+  const signingKey = "EpfaLRpQL9Iq6LuFbvbkrJICC5Qt4cZR8tkFmoumEvw=";
+
+  const expectedSignature = createHmac("sha256", signingKey)
+    .update(JSON.stringify(req.body))
+    .digest("base64");
+
+  if (signature !== expectedSignature) {
+    return res.status(400).json({
+      message: "Invalid signature",
+    });
+  }
+
+  const userId = req.body.contact.id; //this is the identifier for the respond.io API to use anyways
+  const messageText = req.body.message.message.text; //message to send to FRIDAY
+  const channelId = req.body.message.channelId;
+  const customerFirstName = req.body.contact.firstName; //this is to show Dashcam which user they need to reply to in event of no answer
+
+  console.log(`Received message from ${userId}: ${messageText}`);
+  console.log("This is the channelId" + " " + channelId);
+  console.log("This is the assignee" + " " + assignee);
+
   //asking FRIDAY
   let requestBody = {
     chat_history: [],
@@ -69,12 +106,12 @@ app.post("/respond.io", async (req, res) => {
       company_desc: "DashcamSG, a company that sells car accessories",
       company_name: "DashcamSG",
       product_list: ["A500s"],
-      tools: ["VectorDatabase"],
+      tools: ["PriceList", "Schedule", "VectorDatabase"],
       usable_tools: ["VectorDatabase"],
     },
   };
 
-  const apiEndpoint = "http://18.183.218.48/test";
+  const apiEndpoint = "http://18.183.218.48/test"; //FRIDAY endpoint
   const question = messageText;
 
   const encodedQuestion = encodeURIComponent(question);
@@ -84,7 +121,9 @@ app.post("/respond.io", async (req, res) => {
   let answer = responseAI.data.answer; //FRIDAY's ANSWER
 
   const success =
-    !answer.includes("[NO ANSWER]") && !answer.includes("Flagged as");
+    !answer.includes("[NO ANSWER]") &&
+    !answer.includes("Flagged as") &&
+    !answer.includes("context");
 
   //Calling our mongoDB
   let currentHistory = [
@@ -97,6 +136,8 @@ app.post("/respond.io", async (req, res) => {
       content: answer,
     },
   ];
+
+  //checking the previous failure count to update the current failure count
   const prevQuery = await Query.findOne({ userId: userId }).sort({
     createdAt: -1,
   });
@@ -111,6 +152,7 @@ app.post("/respond.io", async (req, res) => {
     question: question,
     answer: answer,
     userId: userId,
+    customerFirstName: customerFirstName,
     success: success,
     history: currentHistory,
     company: "DashcamSG",
@@ -122,16 +164,25 @@ app.post("/respond.io", async (req, res) => {
     .then()
     .catch((e) => console.log(e));
 
-  const mongoCustomer = await Query.findOne({
-    //want to get the Message Object from MongoDB to check number of failures
-    question: question.toString(),
-  }).sort({
+  //Checking how many messages we failed to answer this customer so we can flag out to DashcamSG
+  //we will assign the conversation to DashcamSG if there are more than 2 failed responses
+  const mongoCustomer = await Query.findOne({ user: userId }).sort({
     createdAt: -1,
   });
+
+  //log failure count
+  console.log(
+    "Initial mongoCustomer.failureCount:",
+    mongoCustomer.failureCount
+  );
 
   // Update the count of consecutive failed responses for this user
   if (!success) {
     mongoCustomer.failureCount++;
+    console.log(
+      "Incremented mongoCustomer.failureCount:",
+      mongoCustomer.failureCount
+    );
     await mongoCustomer.save();
   } else {
     // Reset the count if the response was successful
@@ -146,10 +197,8 @@ app.post("/respond.io", async (req, res) => {
     answer =
       "We did not get your question, please hold as our team will be with you shortly.";
   }
-  //yeah then if more than 2 failures we just leave it open and wait for DashcamSG team to reply
-
-  // Send a reply to the incoming message
-
+  // Send a reply to the incoming message using Respond.io
+  // If more than 2 failures we just leave it open and wait for DashcamSG team to reply
   try {
     if (mongoCustomer.failureCount <= 3) {
       const response = await axios.post(
@@ -173,12 +222,28 @@ app.post("/respond.io", async (req, res) => {
         "Reply sent successfully. Message ID:",
         response.data.messageId
       );
+    } else {
+      // Assign the conversation to jarvis@dashcam.sg (eventually)
+      await axios.post(
+        `${apiUrl}/contact/id:${userId}/conversation/assignee`,
+        {
+          assignee: "ask.fridaytech@gmail.com",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Assigned conversation to ask.fridaytech@gmail.com");
     }
   } catch (error) {
-    console.error("Failed to send reply:", error.response.data);
+    console.error(
+      "Failed to send reply or assign conversation:",
+      error.response.data
+    );
   }
 
-  //   // Close the conversation if the failure count is less than 2
   try {
     if (mongoCustomer.failureCount < 2) {
       // Reset the count on MongoDB
@@ -201,9 +266,7 @@ app.post("/respond.io", async (req, res) => {
     console.error(`Error closing conversation for user ${userId}:`, error);
     // Handle error, for example by sending a response with a status code
   }
-
-  // Respond to the request to acknowledge receipt
-
+  // Respond to the webhook request to acknowledge receipt
   res.status(200).end();
 });
 
