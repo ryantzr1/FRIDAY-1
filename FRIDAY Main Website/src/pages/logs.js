16685838fd6df3c69c11e071d764baa8f5b92ef8
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-05-22 00:00:38 +0800

    Add files via upload

--- FRIDAY Main Website/src/pages/logs.js
+++ FRIDAY Main Website/src/pages/logs.js
@@ -7,21 +7,21 @@ import IncomingRequestsCard from "../components/IncomingRequestsCard";
 function LogsPage() {
   const [logs, setLogs] = useState([]);
   const { getUid } = useAuth();
-  
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = getUid() === "lZLIC6fK2WQOvIxyXKECEjx625w1"  || getUid() === "Hoz3NtloWXX7MciVcTn8BNAHIJs1"
-        ? await axios.get("https://friday-backend-server-new.herokuapp.com/queries/log")
-        : null;
-      const { queries } = response.data;
-      setLogs(queries);
+      if (getUid() && (getUid() === "lZLIC6fK2WQOvIxyXKECEjx625w1" || getUid() === "Hoz3NtloWXX7MciVcTn8BNAHIJs1")) {
+        const response = await axios.get("https://friday-backend-server.herokuapp.com/queries/log");
+        const { queries } = response.data;
+        setLogs(queries);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-screen">
-      <h1 className="text-4xl font-bold py-8">Logs</h1>
+    <div className="max-w-7xl mx-auto px-4 pt-8 sm:px-6 lg:px-8 h-screen">
+      <h1 className="text-3xl font-bold mb-4">Logs</h1>
       <div className="h-full">
         <IncomingRequestsCard logs={logs} />
       </div>
