7e45704618f8c13f7ae2f4ac9832356fbda32b0e
ryantzr1 <dreisprung1821@gmail.com>
2023-05-28 18:50:47 +0800

    please work

--- DemExchange.js
+++ DemExchange.js
@@ -75,17 +75,26 @@ async function onMessage(msg) {
 
   const success = !answer.includes("[NO ANSWER]");
 
+  // Split the answer into lines with a maximum line length
+  const maxLineLength = 80; // Adjust as needed
   let response = "";
-  let paragraphs = answer.split("\n\n"); // Split the answer into paragraphs
-
-  for (let i = 0; i < paragraphs.length; i++) {
-    response += paragraphs[i].trim(); // Add the paragraph to the response
-    if (i !== paragraphs.length - 1) {
-      response += "\n\n"; // Add line breaks after each paragraph
+  let currentLine = "";
+  const sentences = answer.split(". "); // Split the answer into sentences
+  for (const sentence of sentences) {
+    if (currentLine.length + sentence.length <= maxLineLength) {
+      currentLine += sentence + ". "; // Add the sentence to the current line
+    } else {
+      if (currentLine.length > 0) {
+        response += currentLine.trim() + "\n"; // Add the current line to the response with line break
+      }
+      currentLine = sentence + ". "; // Start a new line with the sentence
     }
   }
+  if (currentLine.length > 0) {
+    response += currentLine; // Add the last line to the response without line break
+  }
 
-  bot.sendMessage(chatId, response.trim() + "\n");
+  bot.sendMessage(chatId, response);
 
   let currentHistory = [
     {
