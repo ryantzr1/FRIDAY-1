d8595497d59ec64bd05f29bdabaa37dc4e4134d4
Gerald Ng <geraldngjx@gmail.com>
2023-05-29 01:03:46 +0800

    Added loading spinner

--- FRIDAY Main Website/src/pages/index.js
+++ FRIDAY Main Website/src/pages/index.js
@@ -52,6 +52,7 @@ function Home() {
   const [totalQueries, setTotalQueries] = useState(0);
   const [unansweredQueries, setUnansweredQueries] = useState(0);
   const [logs, setLogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // New loading state
   const { getUid } = useAuth();
 
   useEffect(() => {
@@ -60,15 +61,14 @@ function Home() {
         if (getUid() && (getUid() === "lZLIC6fK2WQOvIxyXKECEjx625w1" || getUid() === "Hoz3NtloWXX7MciVcTn8BNAHIJs1")) {
           const response = await axios.get('https://friday-backend-server-new.herokuapp.com/queries/log');
           const { queries, totalQueriesCount, unansweredQueriesCount } = response.data;
-          console.log(queries);
-          console.log(totalQueriesCount);
-          console.log(unansweredQueriesCount);
           setLogs(queries);
           setTotalQueries(totalQueriesCount);
           setUnansweredQueries(unansweredQueriesCount);
         }
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false); // Set loading state to false after data is fetched (or in case of error)
       }
     }
     fetchData();
@@ -81,7 +81,16 @@ function Home() {
         <CounterCard value={totalQueries} label="Total Queries" color="text-green-500" />
         <CounterCard value={unansweredQueries} label="Unanswered Queries" color="text-red-500" />
       </div>
-      <IncomingRequestsCard logs={logs} />
+      {isLoading ? ( // Render loading spinner if isLoading is true
+        <div className="flex items-center justify-center">
+          <svg className="animate-spin h-8 w-8 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+      ) : (
+        <IncomingRequestsCard logs={logs} />
+      )}
     </main>
   );
 }
