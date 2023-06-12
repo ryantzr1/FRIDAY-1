d3f9cab279c1075a7dbf1bf820c7c02f7a46b7a4
ryantzr1 <dreisprung1821@gmail.com>
2023-06-12 12:06:35 +0800

    changed APIendpoints

--- deleteMongoTest.js
+++ deleteMongoTest.js
@@ -11,8 +11,7 @@ async function deleteTest(modelName, query) {
 
   const FRIDAYTestUsers = [86028224, 86025057, 85788878];
   const updatedQuery = { ...query, userId: { $in: FRIDAYTestUsers } };
-   
-  
+
   try {
     const result = await Model.deleteMany(updatedQuery);
     console.log(`Deleted ${result.deletedCount} items`);
